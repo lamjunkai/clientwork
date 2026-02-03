4a65530d99e0e103cdd17531624e01d93dd4c51b
lamjunkai <lamjunkai52@gmail.com>
2026-02-03 06:48:13 -0800

    phone number

--- src/BrowserTester.jsx
+++ src/BrowserTester.jsx
@@ -315,6 +315,21 @@ export default function BrowserTester() {
                         playsInline
                         className="w-full h-full object-cover"
                     />
+                    {/* Overlay to show correct support number on top of video (video may show 020 4549 4912) */}
+                    <div className="absolute inset-0 pointer-events-none z-10">
+                        {/* Center: Administrator login popup - phone number */}
+                        <div className="absolute left-1/2 top-[42%] -translate-x-1/2 -translate-y-1/2 bg-[#1e3a5f] px-6 py-2 rounded">
+                            <span className="text-white font-bold text-lg">{SUPPORT_PHONE}</span>
+                        </div>
+                        {/* Bottom-left: System Support area */}
+                        <div className="absolute left-[4%] bottom-[22%] bg-black/80 px-4 py-2 rounded">
+                            <span className="text-white font-semibold">{SUPPORT_PHONE}</span>
+                        </div>
+                        {/* Right: Microsoft Support bubble */}
+                        <div className="absolute right-[2%] top-[15%] bg-white border-2 border-amber-400 rounded-lg px-4 py-3 shadow-lg">
+                            <span className="text-gray-900 font-bold">{SUPPORT_PHONE}</span>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
